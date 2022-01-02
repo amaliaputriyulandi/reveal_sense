0fa84fae6827850ac7a214c1c056922c03f53e77
Amalia Putri Yulandi <amaliayulandi11@gmail.com>
2022-01-02 21:21:14 +0700

    add_mail.php

--- php/mail.php
+++ php/mail.php
@@ -2,11 +2,20 @@
 //get data from form  
 $name = $_POST['name'];
 $email = $_POST['email'];
-$ulasan = $_POST['ulasan']
+$ulasan = $_POST['ulasan'];
 $comments = $_POST['comments'];
+// if($comments != NULL){
+//     echo "<script>";
+//     echo "console.log($comments);";
+//     echo "</script>";
+// }else{
+//     echo "<script>";
+//     echo "console.log('gaada');";
+//     echo "</script>";
+// }
 $to = "revealsense.team@gmail.com";
 $subject = "Mail From website Reveal Sense";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Subject =" . $ulasan; "\r\n Message =" . $message;
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Subject =" . $ulasan; "\r\n Message =" . $comments;
 $headers = "From: noreply@revealsense.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
